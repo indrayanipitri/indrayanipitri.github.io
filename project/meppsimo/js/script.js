57d815d44a8a2df3a06ee1a27231b5c4b30c725e
indrayanipitri <indrayanipitri@gmail.com>
2020-08-27 10:15:35 +0700

    menambahkan img product

---
+++ project/meppsimo/js/script.js
@@ -0,0 +1,18 @@
+// $(function () {
+
+// 	$(".product-hidden").slice(0, 4).show();
+// 	$("#loadMore").on('click', function (e) {
+
+// 		e.preventDefault();
+
+// 		$(".product-hidden:hidden").slice(0, 4).slideDown();
+// 		if ($(".product-hidden:hidden").length == 0){
+// 			$("#load").fadeOut('slow');
+// 		}
+
+// 		$('html,body').animate({
+// 			scrollTop: $(this).offset().top
+// 		}, 1500);
+
+// 	});
+// });
